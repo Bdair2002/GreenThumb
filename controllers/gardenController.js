e3cd630f302a51a6d902e4589bbc762f3b065e1f
YazanM23 <yazan.mansour2003@gmail.com>
2024-05-14 19:14:21 +0300

    search add delete gardens

--- controllers/gardenController.js
+++ controllers/gardenController.js
@@ -1,9 +1,69 @@
 const db = require('./../models/gardenModel');
+const catchAsync = require('../utils/catchAsync');
 const Garden = db.Garden;
-const addProduct = async (req, res) => {
-  const user = await Garden.create({ name: 'test2', OwnerID: 1 });
-  res.status(200).send(user);
+
+addGarden = catchAsync(async (req, res, next) => {
+  const {
+    Name,
+    Location,
+    Plots,
+    Sunlight,
+    SoilType,
+    WaterSource,
+    Latitude,
+    Longitude,
+  } = req.body;
+
+  const newGarden = await Garden.create({
+    Name: Name,
+    owner_id: req.user.id,
+    Location: Location,
+    Plots: Plots,
+    Sunlight: Sunlight,
+    SoilType: SoilType,
+    WaterSource: WaterSource,
+    Latitude: Latitude,
+    Longitude: Longitude,
+  });
+  res.status(200).send(newGarden);
+});
+getAllGardens = (req, res) => {
+  Garden.findAll().then((gardens) => {
+    res.status(200).send(gardens);
+  });
 };
+
+getMyGardens = catchAsync(async (req, res, next) => {
+  const myGarden = await Garden.findOne({
+    where: { owner_id: req.user.id },
+  });
+  res.status(200).send(myGarden);
+});
+getGardenByName = catchAsync(async (req, res, next) => {
+  Name = req.body.Name;
+  const garden = await Garden.findOne({
+    where: { Name: Name },
+  });
+  res.status(200).send(garden);
+});
+
+deleteGarden = catchAsync(async (req, res, next) => {
+  currentUser = req.user.id;
+  Name = req.body.Name;
+  const garden = await Garden.destroy({
+    where: { Name: Name, owner_id: currentUser },
+  });
+  res.status(204).send({
+    status: 'Delete done.',
+    data: null,
+  });
+});
 module.exports = {
-  addProduct,
+  addGarden,
+  getAllGardens,
+  getMyGardens,
+  getGardenByName,
+  deleteGarden,
 };
+//user
+//create garden //get gardens // get gerdent
