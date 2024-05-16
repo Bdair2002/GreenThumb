1ca3fb2379a3e49ad12fedaa6ee072835df9038a
KariemAbuAisheh <kariemalwazany@gmail.com>
2024-05-16 12:09:31 +0300

    pulling

--- app.js
+++ app.js
@@ -8,15 +8,8 @@ const globalErrorHandler = require('./controllers/errorController');
 const app = express();
 app.use(express.json({ limit: '10kb' }));
 
-<<<<<<< Updated upstream
 app.use('/GreenThumb/v1/users', userRouter);
 app.use('/GreenThumb/v1/gardens', gardenRouter);
-=======
-app.use('/users', userRouter);
-app.use('/gardens', gardenRouter);
-//app.use('/events', eventsRouter);
-//app.use('/resources', resourcesRouter);
->>>>>>> Stashed changes
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
