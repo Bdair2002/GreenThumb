e30981e9fc3f2cb7c9aeafe12efe6236e46ed044
mahmoud arafat <mahmoudarafat0242@gmail.com>
2024-05-17 22:31:10 +0300

    communication garden Features with extrnal api

--- controllers/externalapiController.js
+++ controllers/externalapiController.js
@@ -1,57 +1,51 @@
-const axios = require('axios');
 const db = require('./../models/gardenModel');
+const catchAsync = require('../utils/catchAsync');
+const axios = require('axios');
 const Garden = db.Garden;
 const API_KEY = "cb97d46d38b63b09a5d5e5b64a237da2"; 
 
-
 exports.weatherlatlon = async (req, res) => {
     try {
-        const extrnalApi = await Garden.findAll({
+        const { id } = req.body;
+
+        const garden = await Garden.findOne({
+            where: { id },
             attributes: ['owner_id', 'Location', 'Latitude', 'Longitude'],
-            order: [['owner_id', 'ASC']]
         });
 
-        if (extrnalApi.length === 0) {
-            return res.status(404).json({ error: 'No gardens found' });
+        if (!garden) {
+            return res.status(404).json({ error: 'Garden not found' });
         }
 
+        const { Latitude, Longitude } = garden;
+        const APIUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${Latitude}&lon=${Longitude}&appid=${API_KEY}&units=metric`;
+        const response = await axios.get(APIUrl);
 
-        const weatherData = await Promise.all(extrnalApi.map(async garden => {
-            const { Latitude, Longitude } = garden;
-            const APIUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${Latitude}&lon=${Longitude}&appid=${API_KEY}&units=metric`;
-            const response = await axios.get(APIUrl);
-            return { garden, weather: response.data };
-        }));
-
-        res.status(200).json(weatherData);
+        res.status(200).json({ garden, weather: response.data });
     } catch (error) {
         console.error('Error fetching weather data:', error);
         res.status(500).json({ error: 'Failed to fetch data' });
     }
 };
 
-
 exports.weathercity = async (req, res) => {
     try {
-        const gardens = await Garden.findAll({
+        const { id } = req.body;
+
+        const garden = await Garden.findOne({
+            where: { id },
             attributes: ['owner_id', 'Location', 'Latitude', 'Longitude'],
-            order: [['owner_id', 'ASC']]
         });
 
-        if (gardens.length === 0) {
-            return res.status(404).json({ error: 'No gardens found' });
+        if (!garden) {
+            return res.status(404).json({ error: 'Garden not found' });
         }
 
-        const weatherDataPromises = gardens.map(async garden => {
-            const { Location } = garden;
-            const APIUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${Location}&appid=${API_KEY}&units=metric`;
-            const response = await axios.get(APIUrl);
-            return { garden, weather: response.data };
-        });
+        const { Location } = garden;
+        const APIUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${Location}&appid=${API_KEY}&units=metric`;
+        const response = await axios.get(APIUrl);
 
-        const weatherData = await Promise.all(weatherDataPromises);
-
-        res.status(200).json(weatherData);
+        res.status(200).json({ garden, weather: response.data });
     } catch (error) {
         console.error('Error fetching weather data:', error);
         res.status(500).json({ error: 'Failed to fetch data' });
--- routes/externalapiRouter.js
+++ routes/externalapiRouter.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const fetchWeatherDataController = require('../controllers/externalapiController'); // Corrected path if necessary
+const fetchWeatherDataController = require('../controllers/externalapiController'); 
+const authController = require('./../controllers/authController');
+
 const router = express.Router();
 
-router.get('/weather/latlon/', fetchWeatherDataController.weatherlatlon);
-router.get('/weather/city/', fetchWeatherDataController.weathercity);
+router.use(authController.protect);
+router.get('/weather/latlon', fetchWeatherDataController.weatherlatlon);
+router.get('/weather/city', fetchWeatherDataController.weathercity);
 module.exports = router;
