18ad05e5d190a56f094d6fe7488363c2e2668262
YazanM23 <yazan.mansour2003@gmail.com>
2024-06-10 00:07:33 +0300

    fixing crops and plots and make rotation

--- controllers/plotsController.js
+++ controllers/plotsController.js
@@ -35,12 +35,21 @@ getPlotByID = catchAsync(async (req, res, next) => {
   res.status(200).send(myPlot);
 });
 updatePlot = catchAsync(async (req, res, next) => {
-  const { garden_id, plot_id, Crop, Available } = req.body;
+  const {
+    garden_id,
+    plot_id,
+    Crop,
+    Expected_Date,
+    Planting_Date,
+    Harvested_Date,
+  } = req.body;
   if (gardenController.check) {
     const updatePlot = await Plots.update(
       {
         Crop: Crop,
-        Available: Available,
+        Expected_Date: Expected_Date,
+        Planting_Date: Planting_Date,
+        Harvested_Date: Harvested_Date,
       },
       {
         where: { Garden_ID: garden_id, Plot_ID: plot_id },
@@ -85,6 +94,93 @@ deletePlot = catchAsync(async (req, res, next) => {
   }
 });
 
+getRotation = catchAsync(async (req, res, next) => {
+  plot_id = req.body.plot_id;
+  const crops = await Crops.findAll({
+    where: {
+      Plot_ID: plot_id,
+    },
+    order: [['Planting_Date', 'ASC']],
+  });
+  res.status(200).send(crops);
+});
+
+plantCrop = catchAsync(async (req, res, next) => {
+  const { Crop, plot_id } = req.body;
+  const available = await Plots.findOne({ where: { Plot_ID: plot_id } });
+  if (available.Available) {
+    const currentDate = new Date();
+
+    Planting_Date = currentDate;
+
+    if (gardenController.check) {
+      const updatePlot = await Plots.update(
+        {
+          Crop: Crop,
+          Available: false,
+          Planting_Date: Planting_Date,
+          Garden_ID: available.Garden_ID,
+          Harvested: false,
+        },
+        {
+          where: { Plot_ID: plot_id },
+        },
+      );
+      const getGradenID = await Plots.findOne({ where: { Plot_ID: plot_id } });
+      const updateCrop = await Crops.create({
+        Plot_ID: plot_id,
+        Garden_ID: getGradenID.Garden_ID,
+        Planting_Date: Planting_Date,
+        Harvested: false,
+        Type: Crop,
+      });
+      res.status(201).send(updateCrop);
+    } else {
+      res.status(401).send('You are not authorized to update this plot');
+    }
+  } else {
+    res.status(401).send('Already Planted');
+  }
+});
+
+harvestPlot = catchAsync(async (req, res, next) => {
+  const { plot_id } = req.body;
+  planted = await Plots.findOne({ where: { Plot_ID: plot_id } });
+  if (!planted.Available) {
+    const currentDate = new Date();
+
+    Harvested_Date = currentDate;
+
+    if (gardenController.check) {
+      const updatePlot = await Plots.update(
+        {
+          Available: true,
+          Crop: null,
+          Harvested_Date: Harvested_Date,
+        },
+        {
+          where: { Plot_ID: plot_id },
+        },
+      );
+
+      const updateCrop = Crops.update(
+        {
+          Harvested_Date: Harvested_Date,
+          Harvested: true,
+        },
+        {
+          where: { Plot_ID: plot_id, Harvested: false },
+        },
+      );
+      res.status(201).send(updateCrop);
+    } else {
+      res.status(401).send('You are not authorized to update this plot');
+    }
+  } else {
+    res.status(401).send('Not Planted');
+  }
+});
+
 module.exports = {
   addPlots,
   getAllPlots,
@@ -92,6 +188,9 @@ module.exports = {
   getPlotByGardenID,
   getPlotByID,
   deletePlot,
+  plantCrop,
+  harvestPlot,
+  getRotation,
 };
 
 //search by garden id
